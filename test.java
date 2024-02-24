5995ecee9468aaadde53d206e794eaa60522afb7
Vithushayini <vithushayini2000@gmail.com>
2024-02-24 11:00:55 +0530

    test try 3

--- test.java
+++ test.java
@@ -1,5 +1,11 @@
 
 
 public class test {
-    System.out.println("fghj");
+//    System.out.println("fghj");
+//    test tyr
+
+    public static void main(String[] args){
+        System.out.println("ghj");
+    }
+
 }
